45b70e6dfc014887ae67cde6abdd984dfc77e317
Ryan Chiapoco <43763814+rcswiggy98@users.noreply.github.com>
2019-09-22 17:19:41 -0500

    Change screen resolution

--- src/config/config.js
+++ src/config/config.js
@@ -7,8 +7,8 @@ export default {
         mode: Phaser.Scale.FIT,
         autoCenter: Phaser.Scale.CENTER_BOTH
     },
-  width: 800,
-  height: 600,
+  width: 1920,
+  height: 1080,
   physics: {
     default: 'arcade',
     arcade: {

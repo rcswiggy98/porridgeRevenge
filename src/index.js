8689ea9be5e24e6299313bc9619c57d29544d87d
Ryan Chiapoco <rchiapoco@utexas.edu>
2019-10-20 11:48:59 -0500

    added Level 3

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 /*global Phaser, window*/
 import Level1 from './scenes/Level1.js';
 import Level2 from './scenes/Level2.js';
+import Level3 from './scenes/Level3.js';
 import PickLevel from './scenes/PickLevel.js';
 import Boot from './scenes/Boot.js'
 import GameWinScene from './scenes/GameWinScene.js'
@@ -13,11 +14,12 @@ class Game extends Phaser.Game {
     super(Config);
     this.scene.add('Level1', Level1);
     this.scene.add('Level2', Level2);
+    this.scene.add('Level3', Level3);
     this.scene.add('PickLevel', PickLevel);
     this.scene.add('Boot', Boot);
     this.scene.add('GameOverScene', GameOverScene);
     this.scene.add('GameWinScene', GameWinScene);
-    this.scene.start('Boot');
+    this.scene.start('Level3');
   }
 }
 

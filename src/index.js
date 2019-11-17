5224503bdf20154e96c968e9a684db0bf1a2162e
lihaoyang411 <31487170+lihaoyang411@users.noreply.github.com>
2019-11-17 17:25:23 -0600

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@
 import LevelT from './scenes/LevelT.js';
 import Level1 from './scenes/Level1.js';
 import Level2 from './scenes/Level2.js';
-import Level3 from './scenes/Level3.js';
+// import Level3 from './scenes/Level3.js';
 import Level4 from './scenes/Level4.js';
 import PickLevel from './scenes/PickLevel.js';
 import Boot from './scenes/Boot.js'
@@ -17,7 +17,7 @@ class Game extends Phaser.Game {
     this.scene.add('LevelT', LevelT);
     this.scene.add('Level1', Level1);
     this.scene.add('Level2', Level2);
-    this.scene.add('Level3', Level3);
+    // this.scene.add('Level3', Level3);
     this.scene.add('Level4', Level4);
     this.scene.add('PickLevel', PickLevel);
     this.scene.add('Boot', Boot);
